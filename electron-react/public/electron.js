311827da946bed2970224e7083a73f601f6ce554
rlazcano <rlazcano@redlab.com.mx>
2020-01-30 18:53:24 -0600

    se agrega package npm init ,se verifica que si se pueden leer archivos .ini desde electron

--- electron-react/public/electron.js
+++ electron-react/public/electron.js
@@ -1,4 +1,6 @@
 const electron = require('electron');
+const fs = require('fs');
+const ini = require('ini');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
 
@@ -8,12 +10,22 @@ const isDev = require('electron-is-dev');
 let mainWindow;
 
 function createWindow() {
-  mainWindow = new BrowserWindow({width: 1200, height: 680});
+  mainWindow = new BrowserWindow({ width: 1200, height: 680 });
   mainWindow.loadURL(isDev ? 'http://localhost:3000' : `file://${path.join(__dirname, '../build/index.html')}`);
   if (isDev) {
-    // mainWindow.webContents.openDevTools();
+    mainWindow.webContents.openDevTools();
   }
   mainWindow.on('closed', () => mainWindow = null);
+  try {
+    console.log(__dirname+'\\eva.ini')
+    const config = ini.parse(fs.readFileSync(__dirname+'//eva.ini', 'utf-8'));
+    const degreeType = config.degreeType;
+    console.log(degreeType);
+
+
+  } catch (error) {
+    console.log(error);
+  }
 }
 
 app.on('ready', createWindow);
